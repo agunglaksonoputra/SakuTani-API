0fee57276d80b5a01191b2ecdbce3662d3a52f6a
agunglaksonoputra <alpsosial@gmail.com>
2025-07-05 18:09:49 +0700

    feat: integrate Excel import feature and update sales transaction logic

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const authHeader = req.headers["authorization"];
 
-  if (!token) return res.status(403).json({ message: "No token provided" });
+  if (!authHeader) return res.status(403).json({ message: "No token provided" });
+
+  const token = authHeader.split(" ")[1]; // Ambil hanya token setelah 'Bearer'
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
