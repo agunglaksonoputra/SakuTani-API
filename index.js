52156fe7023b7c4bd2881fe95ce251d4584eff16
agunglaksonoputra <alpsosial@gmail.com>
2025-07-13 17:39:36 +0700

    Change variabel env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: process.env.ENV_FILE || ".env",
+});
 const express = require("express");
 const cors = require("cors");
 const app = express();
